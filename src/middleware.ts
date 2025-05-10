5148978b5673d6f1182a4489493899a6a38be131
Zakariae Bakkari <zakariabakkari2006@gmail.com>
2025-05-10 01:01:05 +0100

    apdate .env

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   return NextResponse.redirect(new URL('/home', request.url))
 }
+
  
 // See "Matching Paths" below to learn more
 export const config = {
